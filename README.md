cfa9f4ed05e8a5e96f0186b546e32f27ace72894
Norton_YR_Li <nortonlyr@gmail.com>
2020-07-29 00:04:00 -0400

    NYC Bus Live Map

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Kafka_Live_Map
+
+In this repo, kafka is used for tracking the route of the designed buslines.
+
+When we run the busdata producers python files, you will see 3 different moving spots in the map. 
+
+
+Environment & tools:
+
+- Python 3.70 (pykafka, flask, JSON)
+- Kafka
+- Javascript (Leaflet.JS)
+
+
+references:
+https://github.com/code-and-dogs/liveMaps
+
+
+![Map_1](img/pic_1.png)
+![Map_2](img/pic_2.png)
+![Map_3](img/pic_3.png)
---
+++ busdata3.py
@@ -0,0 +1,47 @@
+from pykafka import KafkaClient
+import json
+from datetime import datetime
+import uuid
+import time
+
+# Extract coordinate from Geojson)
+input_file = open('bus3_nyc.json')
+json_arr = json.load(input_file)
+coords = json_arr['features'][0]['geometry']['coordinates']
+# print(coords)
+
+#uuid
+def generate_uuid():
+    return uuid.uuid4()
+
+#Kafka Producer
+client = KafkaClient(hosts="localhost:9092")
+topic = client.topics['geodata_final']
+producer = topic.get_sync_producer()
+
+
+#Message
+data = {}
+data['busline'] = '00003'
+
+def generate_checkpoint(coord):
+    i = 0
+    while i < len(coords):
+        data['key'] = data['busline'] + '_' + str(generate_uuid())
+        data['timestamp'] = str(datetime.utcnow())
+        data['latitude'] = coords[i][1]
+        data['longitude'] = coords[i][0]
+
+        message = json.dumps(data)
+        print(message)
+        producer.produce(message.encode('ascii'))
+        time.sleep(1)
+
+        # if bus reaches last coordinate, start from beginning
+        if i == len(coords)-1:
+            i=0
+        else:
+            i += 1
+
+generate_checkpoint(coords)
+
